c6a4fe30d0dabd08a38c8baa0313501c5a25e0fd
ibegel <59898969+ibegel@users.noreply.github.com>
2021-07-05 15:01:33 -0500

    .

---
+++ SACOV_App/SACOV_App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SACOV_App.MvcApplication" Language="C#" %>
---
+++ SACOV_App/SACOV_App/JS/Usuarios.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+    crearListado();
+}
+
+function crearListado()
+{
+    fetch("http://192.168.1.126:8082/api/Usuario")
+        .then(res => res.json())
+        .then(res => {
+            crearListado(res);
+        });
+    document.getElementById("prueba").innerHTML = "<h2>" + res[1].id_Usuario + "</h2>";
+   
+}
