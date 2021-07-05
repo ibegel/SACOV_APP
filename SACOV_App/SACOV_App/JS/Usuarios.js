6e9dd7cc92cdd6a7e0f0d54e54ea411509138041
ibegel <59898969+ibegel@users.noreply.github.com>
2021-07-05 15:32:16 -0500

    Usuario Lista

--- SACOV_App/SACOV_App/JS/Usuarios.js
+++ SACOV_App/SACOV_App/JS/Usuarios.js
@@ -1,14 +1,43 @@
 ﻿window.onload = function () {
-    crearListado();
+    listarUsuario();
 }
 
-function crearListado()
-{
+function listarUsuario() {
     fetch("http://192.168.1.126:8082/api/Usuario")
         .then(res => res.json())
         .then(res => {
             crearListado(res);
         });
-    document.getElementById("prueba").innerHTML = "<h2>" + res[1].id_Usuario + "</h2>";
-   
+}
+
+function crearListado(res) {
+    var contenido = "";
+    contenido += "<table class='table table-hover table-dark'>";
+    contenido += "<thead class='thead-dark'>"
+    contenido += "<tr>";
+    contenido += "<td>Id_Usuario</td>";
+    contenido += "<td>Clave</td>";
+    contenido += "<td>Rango</td>";
+    contenido += "<td>Nombre</td>";
+    contenido += "<td>Operaciones</td>";
+    contenido += "</tr>";
+    contenido += "</thead>"
+    contenido += "<tbody>";
+    for (var i = 0; i < res.length; i++) {
+        contenido += "<tr>";
+        contenido += "<td>" + res[i].id_Usuario + "</td>";
+        contenido += "<td>" + res[i].clave + "</td>";
+        contenido += "<td>" + res[i].rango + "</td>";
+        contenido += "<td>" + res[i].nombre + "</td>";
+        contenido += "<td>";
+        contenido += "<button onclick='AbrirModal(" + res[i].id_Usuario + ")' type='button' class='btn btn-outline-light btn-sm' data-bs-toggle='modal' data-bs-target='#exampleModal'>Editar</button>";
+        contenido += "<button onclick='Eliminar(" + res[i].id_Usuario + ")' type='button' class='btn btn-outline-light btn-sm'>Eliminar</button>";
+        contenido += "</td>";
+
+        contenido += "</tr>";
+    }
+
+    contenido += "</tbody>";
+    contenido += "</table>";
+    document.getElementById("divTabla").innerHTML = contenido;
 }
