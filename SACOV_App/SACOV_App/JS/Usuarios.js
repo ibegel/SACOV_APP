ef53c0c8a85115bb1170d83ea8dcff2a14362ac6
ChrisGarcia <ibegel80@gmail.com>
2021-07-08 15:58:46 -0500

    cambios

--- SACOV_App/SACOV_App/JS/Usuarios.js
+++ SACOV_App/SACOV_App/JS/Usuarios.js
@@ -3,41 +3,28 @@
 }
 
 function listarUsuario() {
-    fetch("http://192.168.1.126:8082/api/Usuario")
+    fetch("http://192.168.100.10:8081/api/Usuario")
         .then(res => res.json())
         .then(res => {
-            crearListado(res);
+            for (var i = 0; i < res.length; i++) {
+                crearListado(res,i);
+            }
         });
 }
 
-function crearListado(res) {
-    var contenido = "";
-    contenido += "<table class='table table-hover table-dark'>";
-    contenido += "<thead class='thead-dark'>"
-    contenido += "<tr>";
-    contenido += "<td>Id_Usuario</td>";
-    contenido += "<td>Clave</td>";
-    contenido += "<td>Rango</td>";
-    contenido += "<td>Nombre</td>";
-    contenido += "<td>Operaciones</td>";
-    contenido += "</tr>";
-    contenido += "</thead>"
-    contenido += "<tbody>";
-    for (var i = 0; i < res.length; i++) {
-        contenido += "<tr>";
-        contenido += "<td>" + res[i].id_Usuario + "</td>";
-        contenido += "<td>" + res[i].clave + "</td>";
-        contenido += "<td>" + res[i].rango + "</td>";
-        contenido += "<td>" + res[i].nombre + "</td>";
-        contenido += "<td>";
-        contenido += "<button onclick='AbrirModal(" + res[i].id_Usuario + ")' type='button' class='btn btn-outline-light btn-sm' data-bs-toggle='modal' data-bs-target='#exampleModal'>Editar</button>";
-        contenido += "<button onclick='Eliminar(" + res[i].id_Usuario + ")' type='button' class='btn btn-outline-light btn-sm'>Eliminar</button>";
-        contenido += "</td>";
-
-        contenido += "</tr>";
-    }
-
-    contenido += "</tbody>";
-    contenido += "</table>";
-    document.getElementById("divTabla").innerHTML = contenido;
+function crearListado(res,i) {
+    var cuerpo = document.getElementById('tabla_cuerpo');
+    var lista = document.createElement('tr');
+    
+        lista.innerHTML = `
+                <td>${res[i].id_Usuario}</td>
+                <td>${res[i].clave}</td>
+                <td>${res[i].rango}</td>
+                <td>${res[i].nombre}</td>
+                <td>
+                    <button onclick='AbrirModal(${res[i].id_Usuario})' type='button' class='btn btn-outline-dark btn-sm' data-bs-toggle='modal' data-bs-target='#exampleModal'>Editar</button>
+                    <button onclick='Eliminar(${res[i].id_Usuario})' type='button' class='btn btn-outline-dark btn-sm'>Eliminar</button>
+                </td>`;
+        cuerpo.appendChild(lista);
 }
+   
