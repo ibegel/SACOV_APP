b88d8e274f3277d54b817e7b8a28f039e61cfee7
ibegel <59898969+ibegel@users.noreply.github.com>
2021-09-10 15:34:45 -0500

    CHART

--- SACOV_App/SACOV_App/JS/Agente.js
+++ SACOV_App/SACOV_App/JS/Agente.js
@@ -3,7 +3,7 @@
 }
 
 function listarUsuario() {
-    fetch("http://192.168.100.10:8081/api/Usuario")
+    fetch("http://192.168.1.103:8081/api/Agente")
         .then(res => res.json())
         .then(res => {
             for (var i = 0; i < res.length; i++) {
@@ -22,13 +22,14 @@ function crearListado(res,i) {
     var lista = document.createElement('tr');
     lista.setAttribute("id", "Entidades");
         lista.innerHTML = `
-                <td>${res[i].id_Usuario}</td>
-                <td>${res[i].clave}</td>
-                <td>${res[i].rango}</td>
+                <td>${res[i].id_Agente}</td>
+                <td>${res[i].matricula}</td>
+                <td>${res[i].grado}</td>
                 <td>${res[i].nombre}</td>
+                <td>${res[i].distintivo}</td>
                 <td>
-                    <button onclick='AbrirModal(${res[i].id_Usuario})' type='button' class='btn btn-outline-dark btn-sm' data-bs-toggle='modal' data-bs-target='#exampleModal'>Editar</button>
-                    <button onclick='Eliminar(${res[i].id_Usuario})' type='button' class='btn btn-outline-dark btn-sm'>Eliminar</button>
+                    <button onclick='AbrirModal(${res[i].id_Agente})' type='button' class='btn btn-outline-dark btn-sm' data-bs-toggle='modal' data-bs-target='#exampleModal'>Editar</button>
+                    <button onclick='Eliminar(${res[i].id_Agente})' type='button' class='btn btn-outline-dark btn-sm'>Eliminar</button>
                 </td>`;
         cuerpo.appendChild(lista);
 }
@@ -45,45 +46,48 @@ function AbrirModal(id) {
     Limpiar();
     if (id == 0) {
         document.getElementById("tclave").value = "D-";
-        document.getElementById("lbltitulo").innerHTML = "Agregar Doctor";
+        document.getElementById("lbltitulo").innerHTML = "Agregar Agente";
     }
     else {
-        fetch("http://192.168.100.10:8081/api/Usuario?id_Usuario=" + id)
+        fetch("http://192.168.1.103:8081/api/Agente?id_Agente=" + id)
             .then(res => res.json())
             .then(res => {
-                document.getElementById("tid_Usuario").value = res.id_Usuario;
+                document.getElementById("tid_Agente").value = res.id_Agente;
+                document.getElementById("tmatricula").value = res.matricula;
+                document.getElementById("cbogrado").value = res.grado;
                 document.getElementById("tnombre").value = res.nombre;
-                document.getElementById("tclave").value = res.clave;
-                document.getElementById("cborango").value = res.rango;
+                document.getElementById("tdistintivo").value = res.distintivo;
             });
-        document.getElementById("lbltitulo").innerHTML = "Editar Doctor";
+        document.getElementById("lbltitulo").innerHTML = "Editar Agente";
     }
 }
 
 function Guardar() {
     if (confirm("Desea Guardar los Cambios") == 1) {
-        var jid = document.getElementById("tid_Usuario").value;
-        var jclave = document.getElementById("tclave").value;
-        var jrango = document.getElementById("cborango").value;
+        var jid = document.getElementById("tid_Agente").value;
+        var jclave = document.getElementById("tmatricula").value;
+        var jrango = document.getElementById("cbogrado").value;
         var jnombre = document.getElementById("tnombre").value;
+        var jdistintivo = document.getElementById("tdistintivo").value;
         if (jid == null || jid == 0) {
-            fetch("http://192.168.100.10:8081/api/Usuario")
+            fetch("http://192.168.1.103:8081/api/Agente")
                 .then(res => res.json())
                 .then(res => {
-                    jid = res.length + 1;
+                    //jid = res.length + 1;
                 });
         }
-        fetch("http://192.168.100.10:8081/api/Usuario",
+        fetch("http://192.168.1.103:8081/api/Agente",
             {
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 method: 'POST',
                 body: JSON.stringify({
-                    "Id_usuario": jid,
-                    "Clave": jclave,
-                    "Rango": jrango,
-                    "Nombre": jnombre
+                    "Id_Agente": jid,
+                    "Matricula": jclave,
+                    "Grado": jrango,
+                    "Nombre": jnombre,
+                    "Distintivo":jdistintivo
                 })
             }).then(res => res.json())
             .then(res => {
